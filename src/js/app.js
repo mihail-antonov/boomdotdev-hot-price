45edbfdfbd60e65c70ff818473ec1b666e24293c
Mihail Antonov <mihail.d.antonov@gmail.com>
2023-01-17 21:47:57 +0200

    replacement of js from index to app.js and polishing of the foreach method.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+
+  let prices = document.querySelectorAll('.price');
+
+  prices.forEach(price => {
+
+    if (price.classList.contains('hot')) {
+
+      price.append('🔥');
+    }
+  });
 });
